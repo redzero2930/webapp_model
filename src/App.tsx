a414b6c7717ed88084e2053dd90d787c61a27405
@redzero2930 <redzero2930@gmail.com>
2023-08-12 00:08:51 -0600

    test 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import MainPage from "./Pages/Main-page";
+
 function App() {
   return (
     <div className="App">
       <h1>Hola mundo, app_model</h1>
+      <MainPage/>
     </div>
   );
 }
---
+++ src/Pages/Main-page.tsx
@@ -0,0 +1,7 @@
+export function MainPage(){
+    return(
+        <h1>Welcome to the main page</h1>
+    );
+}
+
+export default MainPage;
