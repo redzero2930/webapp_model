4bb8a01609acd4db350d0c88fc52d4541d643f7f
@redzero2930 <redzero2930@gmail.com>
2023-08-12 21:27:04 -0600

    input commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Input from "./Components/Input";
 import MainPage from "./Pages/Main-page";
 
 function App() {
@@ -5,6 +6,11 @@ function App() {
     <div className="App">
       <h1>Hola mundo, app_model, second pc</h1>
       <MainPage/>
+      <Input
+        name="SampleInput"
+        value="SampleInput"
+        placeholder="Value"
+      />
     </div>
   );
 }
--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,5 +1,28 @@
-export function Input(){
-    return(
-        <h1>Bienvenido a Input</h1>
-    );
+import React, { useState } from "react";
+
+interface BaseProps {
+  name: string;
+  value?: string
+  placeholder?: string;
 }
+
+export function Input(props: BaseProps) {
+  const { name, placeholder } = props;
+
+  const [value, setValue] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <input
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={handleInputChange}
+    />
+  );
+}
+
+export default Input;
